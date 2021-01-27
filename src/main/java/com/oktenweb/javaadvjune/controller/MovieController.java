edf70046b3480f77bef10c0ca2d148c010eae1f4
Volodymyr77 <gagarinvov@gmail.com>
2021-01-28 00:07:47 +0200

    c

--- src/main/java/com/oktenweb/javaadvjune/controller/MovieController.java
+++ src/main/java/com/oktenweb/javaadvjune/controller/MovieController.java
@@ -2,6 +2,15 @@ package com.oktenweb.javaadvjune.controller;
 
 import com.oktenweb.javaadvjune.dao.MovieRepository;
 import com.oktenweb.javaadvjune.entity.Movie;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,31 +19,41 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+//@Component
+//@Bean
+//@Controller
+//@Repository
+//@Service
+//@Configuration
+//@Value
+
 @RestController
 @RequestMapping(value = "/movies")
+
 public class MovieController {
 
-    private List<Movie> movies = new ArrayList<>();
+    private final MovieRepository movieRepository;
 
-    {
-
-        movies.add(new Movie(1, "LoTR: BR", 187));
-        movies.add(new Movie(2, "LoTR: TT", 179));
-        movies.add(new Movie(3, "LoTR: RK", 193));
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
     }
 
-
-    private MovieRepository movieRepository;
+//    private List<Movie> movies = new ArrayList<>();
+//
+//    {
+//
+//        movies.add(new Movie(1, "LoTR: BR", 187));
+//        movies.add(new Movie(2, "LoTR: TT", 179));
+//        movies.add(new Movie(3, "LoTR: RK", 193));
+//    }
 
     @GetMapping
     public List<Movie> getAllMovies() {
-
         return movieRepository.findAll();
     }
 
     @PostMapping
     public Movie createMovie(@RequestBody Movie movie) {
-        movies.add(movie);
         return movieRepository.saveAndFlush(movie);
     }
 
@@ -44,6 +63,7 @@ public class MovieController {
     if (movieOptional.isPresent()) {
         throw new IllegalArgumentException("No movie with such id: " +  id);
             }
+    movie.setId(id);
        return movieRepository.saveAndFlush(movie);
     }
 
